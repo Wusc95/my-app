7337a8f40f6eb3d6812de1bffc47798cafe32434
Wusc95 <1223348519@qq.com>
2020-02-24 10:21:02 +0800

    通过defaultProps 设置props里面属性的默认值

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<p>总共有:{this.props.iNum}条数据</p>
+				<p>每页显示:{this.props.iNum}条数据</p>
 				<p>用户性别是:{this.props.gender}</p>
 			</div>
 		)
@@ -19,6 +19,11 @@ App.propTypes = {
 	iNum: propTypes.number.isRequired,
 	gender:propTypes.oneOf(['男','女'])
 }
-ReactDOM.render(<App iNum={10} gender="男"/>, document.getElementById('root'));
+
+// 通过defaultProps 设置props里面属性的默认值
+App.defaultProps={
+	gender:'男'
+}
+ReactDOM.render(<App iNum={10} gender='女'/>, document.getElementById('root'));
 
 
