3f34abb4c3f71ac8faf96d581c36f4bfef8b776d
Wusc95 <1223348519@qq.com>
2020-02-25 09:53:45 +0800

    路由-Switch-Redirect-使用

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Link } from 'react-router-dom'
+import { HashRouter, Route, Link, Switch, Redirect } from 'react-router-dom'
 import './style/main.css'
 function Page01() {
     return (
@@ -17,6 +17,11 @@ function Page03() {
         <h1>页面内容三</h1>
     )
 }
+function NotFound(){
+    return (
+        <h1>抱歉。页面未找到</h1>
+    )
+}
 class App extends Component {
     render() {
         return (
@@ -25,9 +30,15 @@ class App extends Component {
                 <Link to="/page02">页面二</Link>&nbsp;&nbsp;&nbsp;
                 <Link to="/page03">页面三</Link>
                 <hr />
-                <Route exact path="/" component={Page01}></Route>
-                <Route path="/page02" component={Page02}></Route>
-                <Route path="/page03" component={Page03}></Route>
+                <Switch>
+                    <Route exact path="/page01" component={Page01}></Route>
+                    <Route path="/page02" component={Page02}></Route>
+                    <Route path="/page03" component={Page03}></Route>
+
+                    <Redirect exact from='/' to='/page01'/>
+                    <Route component={NotFound}></Route>
+
+                </Switch>
             </HashRouter>
         )
     }
